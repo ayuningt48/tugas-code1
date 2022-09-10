9c968529321939ccae155d5a0a6f1d0da0cc5491
ayuningt48 <98692322+ayuningt48@users.noreply.github.com>
2022-09-10 10:22:14 +0700

    Add files via upload

---
+++ code/tyas.py
@@ -0,0 +1,59 @@
+# print("tri ayuningtyas")
+
+def fungsi_python():
+ print("hai ini adalah python")
+
+nama = "tri ayunintyas"
+umur = 18
+nosepatu = 38.5
+print("nama saya adalah " + nama + " umur saya adalah " + str(umur))
+print("umur saya adalah : " ,umur)
+print("ukuran sepatu saya adalah : " ,nosepatu)
+print(nosepatu,"dia adalah",type(nosepatu))
+
+#boolean
+masukkelas = True
+pulangkelas = False
+
+print(masukkelas)
+print(pulangkelas)
+
+#list <array>
+
+binatang = ["ayam","merpati","kelinci","tupai"]
+namabunga =("anggrek", "melati", "flamboyan")
+namaorang = {"tyas", "ayu"}
+
+print(binatang[3])
+print(binatang[-3])
+
+#operasi aritmatika
+angka1 = 3
+angka2 = 2
+angka3 = 7
+print ("penjumlahan : " + str(angka1 + angka2 + angka3 +20))
+
+print ("pengurangan : " + str(angka1 + angka2 + angka3 -20))
+
+print ("pembagian : " + str(angka1 + angka2 + angka3 /20))
+
+print ("perkalian : " + str(3 * 4 * 5 ))
+
+print ("pembagian 2 : " + str(13 /10))
+
+print ("modulus : " + str(13 % 10))
+
+#tuple
+#append untuk menambahkan value\nilai, remove menghapus, add menabahdata "ning"
+# fungsi_python()
+#  ini komentar
+#  ini bukan komentar
+
+#tugas
+#konversi suhu (2)
+#menghitung bangun ruang (1)
+nama = input("masukkan nama anda :")
+print(  "ternyata nama anda" +nama)
+
+
+
